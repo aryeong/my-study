24e5c7e9010c33e785b2c9226e9c508804d22e5d
aryeong <spaerh7@gmail.com>
2023-05-02 00:20:53 +0900

    [#11] js-30-11 : volume 적용

--- javascript/js-30/11-custom-video-player/scripts.js
+++ javascript/js-30/11-custom-video-player/scripts.js
@@ -4,26 +4,30 @@ class videoPlayer {
   constructor(){
     this.video = document.querySelector('.player__video');
     this.videoBtn = document.querySelector('.player__button');
-    this.videoVolume = document.querySelector('.player__slider');
+    this.range = document.querySelectorAll('.player__slider');
     this.playFunc();
-    this.volumeFunc();
+    this.bindEventFunc();
   };
   playFunc() {
     this.videoBtn.addEventListener('click', () => {
       if (this.video.paused) {
         this.video.play();
-        this.videoBtn.innerText = '►';
+        this.videoBtn.innerText = '❚ ❚';
+        
       } else {
         this.video.pause();
-        this.videoBtn.innerText = '❚ ❚';
+        this.videoBtn.innerText = '►';
       }
     })
   }
-  volumeFunc() {
-    const volumeValue = this.videoVolume.value;
-    this.video.volume = volumeValue;
-    console.log(volumeValue)
+  bindEventFunc() {
+    this.range.forEach(range => range.addEventListener('change', this.rangeFunc));
+
   }
+  rangeFunc = e => {
+    this.video[e.target.name] = e.target.value;
+  }
+  
 }
 
 new videoPlayer();
